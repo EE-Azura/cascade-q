f9b042dcbb2508b60d373544d51ceee503b66cea
EE <ee_azura@outlook.com>
2025-01-30 21:18:55 +0800

    build: init

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+const packageName = 'cascade-q';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'CascadeQ',
+      formats: ['es', 'cjs'],
+      fileName: (format: string) =>
+        format === 'es' ? `${packageName}.js` : `${packageName}.cjs`
+    },
+    sourcemap: true
+  },
+  plugins: [dts()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+});
